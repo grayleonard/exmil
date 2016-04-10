b8f6f3e6f7f7c6c038375aafbd37e617f646cf46
Gray Leonard <gl7039a@american.edu>
2016-04-09 20:14:44 -0400

    new

--- testing/gulpfile.js
+++ testing/gulpfile.js
@@ -1,13 +1,23 @@
 var gulp = require('gulp');
 var mustache = require('gulp-mustache');
+var clean_css = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+var clean = require('gulp-clean');
 
 gulp.task('default', function() {
-	gulp.src('./templates/index.mustache')
+	gulp.src('templates/index.mustache')
 		.pipe(mustache({}, {extension: '.html'}, {}))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist/'));
+	gulp.src('src/css/*.css')
+		.pipe(clean_css())
+		.pipe(gulp.dest('./dist/css/'));
+	gulp.src('src/js/*.js')
+		.pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('copy', function() {
-	gulp.src('src/**/*')
-		.pipe(gulp.dest('./dist/'));
+gulp.task('clean', function() {
+	gulp.src('dist/**/*')
+		.pipe(clean());
 });
