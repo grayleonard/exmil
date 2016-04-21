cc25f23a819ec54112156f5eb30893210dc74d5e
Gray Leonard <gl7039a@american.edu>
2016-04-21 09:30:50 -0400

    fix

--- testing/src/js/control.js
+++ testing/src/js/control.js
@@ -4,7 +4,7 @@ var load_ed_imgs = function(ed, callback) {
 	$.each(imgs, function(idx, ele) {
 		console.log($(ele));
 		var real_url = $(ele).attr('data-echo');
-		if(real_url != undefined && real_url != ele.src)
+		if(real_url != undefined && real_url != ele.src) {
 			ele.src = real_url;
 			$(ele).load(function() {
 				if(idx == imgs.length-1) {
@@ -12,6 +12,7 @@ var load_ed_imgs = function(ed, callback) {
 					callback();
 				}
 			});
+		}
 	});
 }
 var hf = function() {
@@ -25,13 +26,13 @@ var hf = function() {
 			$('.entries').addClass('show').removeClass('dont-show');
 			$('.edition').not(edition).addClass('not-active').removeClass('active');
 			$('#navigation').addClass('show').removeClass('dont-show');
-			$(document).scrollTop( $('#' + page[0]).offset().top-40 );
+			//$(document).scrollTop( $('#' + page[0]).offset().top-40 );
 			if(text != undefined) {
 				console.log(text);
-				$(document).scrollTop( $('#' + text).offset().top );
+			//	$(document).scrollTop( $('#' + text).offset().top );
 				$(window).load(function() {
 					console.log('calling2');
-					$(document).scrollTop( $('#' + text).offset().top );
+			//		$(document).scrollTop( $('#' + text).offset().top );
 				});
 			}
 
